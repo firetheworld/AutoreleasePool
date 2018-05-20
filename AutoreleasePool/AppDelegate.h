04d1527146fb467fc2a7ad51c5f3ca5e031e6cf2
郑良凯 <liangkai.zheng@datayes.com>
2018-05-20 17:13:32 +0800

    init project

---
+++ AutoreleasePool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoreleasePool
+//
+//  Created by 郑良凯 on 2018/5/20.
+//  Copyright © 2018年 datayes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoreleasePool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoreleasePool
+//
+//  Created by 郑良凯 on 2018/5/20.
+//  Copyright © 2018年 datayes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
